2106fbc580db3fdd3ceb25ffd758a614c0def52a
HoneyFox <HoneyFox@fake.fake>
2014-03-05 22:30:40 +0800

    Fix incorrect axes.

--- OrbitManipulator.cs
+++ OrbitManipulator.cs
@@ -42,25 +42,27 @@ namespace NBody
 			{
 				Vessel vessel = kv.Key;
 				Vector3d totalAccOnVessel = kv.Value;
-
-				Vector3d position = vessel.orbit.getRelativePositionAtUT(Planetarium.GetUniversalTime());
-				Orbit orbit2 = new Orbit(vessel.orbit.inclination, vessel.orbit.eccentricity, vessel.orbit.semiMajorAxis, vessel.orbit.LAN, vessel.orbit.argumentOfPeriapsis, vessel.orbit.meanAnomalyAtEpoch, vessel.orbit.epoch, vessel.orbit.referenceBody);
-				orbit2.UpdateFromStateVectors(position, vessel.orbit.vel + new Vector3d(totalAccOnVessel.x, totalAccOnVessel.z, totalAccOnVessel.y) * TimeWarp.fixedDeltaTime, vessel.orbit.referenceBody, Planetarium.GetUniversalTime());
-
-				if (!double.IsNaN(orbit2.inclination) && !double.IsNaN(orbit2.eccentricity) && !double.IsNaN(orbit2.semiMajorAxis)) // && orbit2.timeToAp > TimeWarp.fixedDeltaTime)
+				if ((totalAccOnVessel * TimeWarp.fixedDeltaTime).magnitude >= float.Epsilon)
 				{
-					vessel.orbit.inclination = orbit2.inclination;
-					vessel.orbit.eccentricity = orbit2.eccentricity;
-					vessel.orbit.semiMajorAxis = orbit2.semiMajorAxis;
-					vessel.orbit.LAN = orbit2.LAN;
-					vessel.orbit.argumentOfPeriapsis = orbit2.argumentOfPeriapsis;
-					vessel.orbit.meanAnomalyAtEpoch = orbit2.meanAnomalyAtEpoch;
-					vessel.orbit.epoch = orbit2.epoch;
-					vessel.orbit.referenceBody = orbit2.referenceBody;
-					vessel.orbit.Init();
+					Vector3d position = vessel.orbit.getRelativePositionAtUT(Planetarium.GetUniversalTime());
+					Orbit orbit2 = new Orbit(vessel.orbit.inclination, vessel.orbit.eccentricity, vessel.orbit.semiMajorAxis, vessel.orbit.LAN, vessel.orbit.argumentOfPeriapsis, vessel.orbit.meanAnomalyAtEpoch, vessel.orbit.epoch, vessel.orbit.referenceBody);
+					orbit2.UpdateFromStateVectors(position, vessel.orbit.vel + totalAccOnVessel * TimeWarp.fixedDeltaTime, vessel.orbit.referenceBody, Planetarium.GetUniversalTime());
 
-					//prevVessel.orbit.UpdateFromOrbitAtUT(orbit2, Planetarium.GetUniversalTime(), orbit2.referenceBody);
-					vessel.orbit.UpdateFromUT(Planetarium.GetUniversalTime());
+					if (!double.IsNaN(orbit2.inclination) && !double.IsNaN(orbit2.eccentricity) && !double.IsNaN(orbit2.semiMajorAxis)) // && orbit2.timeToAp > TimeWarp.fixedDeltaTime)
+					{
+						vessel.orbit.inclination = orbit2.inclination;
+						vessel.orbit.eccentricity = orbit2.eccentricity;
+						vessel.orbit.semiMajorAxis = orbit2.semiMajorAxis;
+						vessel.orbit.LAN = orbit2.LAN;
+						vessel.orbit.argumentOfPeriapsis = orbit2.argumentOfPeriapsis;
+						vessel.orbit.meanAnomalyAtEpoch = orbit2.meanAnomalyAtEpoch;
+						vessel.orbit.epoch = orbit2.epoch;
+						vessel.orbit.referenceBody = orbit2.referenceBody;
+						vessel.orbit.Init();
+
+						//prevVessel.orbit.UpdateFromOrbitAtUT(orbit2, Planetarium.GetUniversalTime(), orbit2.referenceBody);
+						vessel.orbit.UpdateFromUT(Planetarium.GetUniversalTime());
+					}
 				}
 			}
 			manipulations.Clear();
